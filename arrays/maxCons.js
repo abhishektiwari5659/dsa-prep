e9c581d3b9b49e3f7677ab44bc61dff0e81cec04
HeheAbhi <abhishekti5659@gmail.com>
2025-11-06 22:12:56 +0530

    max Consecutive and merge sorrted arrays

---
+++ arrays/maxCons.js
@@ -0,0 +1,20 @@
+function max(arr){
+    let count = 0;
+    let maxCount = 0;
+    for(let i =0; i < arr.length; i++){
+        if( arr[i] == 1){
+            count++
+        }
+        else{
+            count = 0
+        }
+
+    }
+    if(count > maxCount){
+        maxCount = count
+    }
+    return maxCount
+}
+
+let arr = [1,0,0,1,1,1,0,0,1,1,1,1,0]
+console.log(max(arr))
---
+++ arrays/mergeSortedArray.js
@@ -0,0 +1,19 @@
+function merSorted(arr1, m, arr2, n) {
+    let p1 = m - 1;
+    let p2 = n - 1
+    for (let i = m + n - 1; i > 0; i--) {
+        if (p2 < 0) break
+        if (p1 >= 0 && arr1[p1] > arr2[p2]) {
+            arr1[i] = arr1[p1]
+            p1--
+        } else {
+            arr1[i] = arr2[p2]
+            p2--
+        }
+    }
+    return arr1
+}
+let arr1 = [1, 2, 3, 0, 0, 0]
+let arr2 = [2, 5, 6]
+
+console.log(merSorted(arr1, 3, arr2, 3))
